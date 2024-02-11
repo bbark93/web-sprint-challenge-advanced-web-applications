583b5d9a036851f9cbf9cadeee95486debfd4a59
Brandon Barksdale <bbark93@gmail.com>
2024-02-11 15:54:08 -0500

    implemented isDisabled

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -16,6 +16,7 @@ export default function LoginForm(props) {
 
   const onSubmit = evt => {
     evt.preventDefault()
+    console.log('values:', values);
     // ✨ implement
   }
 
@@ -24,6 +25,7 @@ export default function LoginForm(props) {
     // Trimmed username must be >= 3, and
     // trimmed password must be >= 8 for
     // the button to become enabled.
+    return !(values.username.trim().length >= 3 && values.password.trim().length >= 8);
   }
 
   return (
