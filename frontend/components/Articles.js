ba39ef4a711b84e3b966d2c37895921eb9f1cd12
Brandon Barksdale <bbark93@gmail.com>
2024-02-13 18:51:27 -0500

    implemented logic to redirect if no token

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -7,6 +7,11 @@ export default function Articles(props) {
 
   // ✨ implement conditional logic: if no token exists
   // we should render a Navigate to login screen (React Router v.6)
+  if (!localStorage.getItem('token')) {
+    return(
+      <Navigate to='/' />
+    );
+  }
 
   useEffect(() => {
     // ✨ grab the articles here, on first render only
